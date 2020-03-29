84342762b63c66f05a885127fb3e2b74c9bd56e9
Nikolas <Nikolas@MacBook.local>
2020-03-29 13:10:42 +0200

    make it better5

--- src/index.js
+++ src/index.js
@@ -8,27 +8,26 @@ let port = process.env.PORT || 80;
 const app = express();
 app.use(cors());
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
 
 app.listen(port);
 console.log("Listening on port " + port);
 
 app.post("/get", async (req, res) => {
-  console.log("Post request for: ", req.body.url);
-  if (typeof req.body.url === "undefined") {
+  console.log("Post request for: ", req.body.data);
+  if (typeof req.body.data === "undefined") {
     res.send("Not allowed");
     return;
   }
-  if (!Array.isArray(req.body.url)) {
+  if (!Array.isArray(req.body.data)) {
     res.send("Not allowed");
     return;
   }
-  if (req.body.url.filter(element => element.length === 0).length > 0) {
+  if (req.body.data.filter(element => element.length === 0).length > 0) {
     res.send("Not allowed");
     return;
   }
 
-  getFeeds(req.body.url).then(feeds => {
+  getFeeds(req.body.data).then(feeds => {
     let consolidatedFeed = [];
     feeds.forEach(feed => (consolidatedFeed = [...consolidatedFeed, ...feed]));
     sortFeed(consolidatedFeed);
@@ -43,36 +42,28 @@ app.post("/get", async (req, res) => {
 const getFeeds = async feeds => {
   let promises = [];
 
-  for (let feed of feeds) promises.push(getFeed(feed));
+  for (let feedObj of feeds) promises.push(getFeed(feedObj));
 
   return Promise.all(promises);
 };
 
-const getFeed = async feed => {
+const getFeed = async feedObj => {
   let promise = new Promise((resolve, reject) => {
-    Feed.load(feed, (error, json) => {
+    Feed.load(feedObj.link, (error, json) => {
       if (error != null) {
-        console.error("Error getting feed: ", feed);
+        console.error("Error getting feed: ", feedObj.link);
         resolve([]);
-      } else resolve(parseFeed(json, feed));
+      } else resolve(parseFeed(json, feedObj));
     });
   });
 
   return promise;
 };
 
-const parseFeed = (jsonFeed, url) => {
-  let newFeed = {};
+const parseFeed = (jsonFeed, feedObj) => {
   let newEntries = [];
-  let avatarThumbnail = "";
 
-  const channelKeys = ["description", "title", "image", "category", "url"];
   const itemKeys = ["title", "description", "link", "pubDate", "category"];
-
-  // Get link for special thumbnail, if part of the default list of feeds
-  for (let item of feedListDrawerTemplate) {
-    if (item.id === url) avatarThumbnail = item.thumbnail;
-  }
 
   if ("items" in jsonFeed) {
     for (let item of jsonFeed["items"]) {
@@ -103,10 +94,10 @@ const parseFeed = (jsonFeed, url) => {
         {
           ...newEntry,
           isFavorite: false,
-          avatarThumbnail: avatarThumbnail,
-          // avatarText: avatarName,
+          avatarThumbnail: feedObj.avatarThumbnail,
+          avatarText: feedObj.avatarText,
           rootTitle: jsonFeed["title"],
-          rootLink: url,
+          rootLink: feedObj.link,
           isRead: false
         }
       ];
@@ -119,57 +110,3 @@ const parseFeed = (jsonFeed, url) => {
 const sortFeed = feed => {
   feed.sort((a, b) => (Date.parse(a.pubDate) < Date.parse(b.pubDate) ? 1 : -1));
 };
-
-const feedListDrawerTemplate = [
-  {
-    name: "NZZ  - Startseite",
-    avatarName: "NZZ",
-    thumbnail:
-      "https://pbs.twimg.com/profile_images/1006460634236628992/dc1hsh9d.jpg",
-    id: "https://www.nzz.ch/startseite.rss"
-  },
-  {
-    name: "FAZ - Aktuell",
-    avatarName: "FAZ",
-    thumbnail:
-      "https://pbs.twimg.com/profile_images/1177121016524550145/KJESjKrB_400x400.jpg",
-    id: "https://www.faz.net/rss/aktuell/"
-  },
-  {
-    name: "Welt - Politik",
-    avatarName: "W",
-    thumbnail:
-      "https://pbs.twimg.com/profile_images/775627854293954561/Y4iLEu_V_400x400.jpg",
-    id: "https://www.welt.de/feeds/section/politik.rss"
-  },
-  {
-    name: "Welt - Wirtschaft",
-    avatarName: "W",
-    thumbnail:
-      "https://pbs.twimg.com/profile_images/775627854293954561/Y4iLEu_V_400x400.jpg",
-    id: "https://www.welt.de/feeds/section/wirtschaft.rss"
-  },
-  {
-    name: "Süddeutsche Zeitung - Topthemen",
-    avatarName: "SZ",
-    thumbnail:
-      "https://pbs.twimg.com/profile_images/655020120121712640/WcX4aKls.png",
-    id: "https://rss.sueddeutsche.de/rss/Topthemen"
-  },
-
-  {
-    name: "Spiegel",
-    avatarName: "S",
-    thumbnail:
-      "https://pbs.twimg.com/profile_images/1214723509521387520/7UENeEVp_400x400.jpg",
-    id: "https://www.spiegel.de/schlagzeilen/index.rss"
-  },
-
-  {
-    name: "New York Times - Homepage",
-    avatarName: "NYT",
-    thumbnail:
-      "https://pbs.twimg.com/profile_images/1098244578472280064/gjkVMelR_400x400.png",
-    id: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-  }
-];
